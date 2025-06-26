4fcbc7b9f924d2dff59634c3834b414c07d3001d
Phil Killewald <reallyasi9@users.noreply.github.com>
2025-06-26 17:54:00 -0400

    Fix typo in documentation

--- src/CMSCertificationNumbers.jl
+++ src/CMSCertificationNumbers.jl
@@ -49,7 +49,7 @@ Medicare providers use six-character identifiers with the following format:
 > `SSPQQQ`
 Where:
 - `SS` represent a two-character alphanumeric State Code;
-- `P` represents either an a literal 'P' character (for Organ Procurement Organizations) or the most significant digit of the Sequence Number;
+- `P` represents either a literal 'P' character (for Organ Procurement Organizations) or the most significant digit of the Sequence Number;
 - `QQQ` represents the three least significant digits of the Sequence Number.
 
 The constructor performs no error checking, but will throw an exception if `n` has more than
